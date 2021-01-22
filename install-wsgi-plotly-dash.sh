c93d7cee4b1642179c5e16d0ef3d6a23ebf382ae
Pierre Veelen (wsgi) <pierre@pvln.nl>
2021-01-22 15:50:09 +0100

    gunicorn added en changend ada to wsgi

--- install-wsgi-plotly-dash.sh
+++ install-wsgi-plotly-dash.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-# ---------------------------- INSTALL NOTES ----------------------------
-# To properly setup the AWS CLI, make sure that this shell script is run with the two required positional args:
-#   $1 :: (string) AWS Access Key ID
-#   $2 :: (string) AWS Secret Key
-#
-# Also make sure to give the script permission to run!:
-#    chmod 755 /home/ubuntu/install.sh
-#
-# -----------------------------------------------------------------------
-
-
 # ++++++++++++++++++++ START ANACONDA INSTALL +++++++++++++++++++++
 #sudo su
 
@@ -28,17 +17,16 @@ source /home/ubuntu/.bashrc
 
 # Create base Python3 environments separate from the base env
 conda update -y -n base -c defaults conda
-conda create -y --name ada38 python=3.8 
+conda create -y --name wsgi38 python=3.8 
 conda create -y --name base38 python=3.8 
 
 # +++++++++++++++++++++ END ANACONDA INSTALL ++++++++++++++++++++++
 
-
-# ++++++++++++++ SETUP ENV +++++++++++++++
+# ++++++++++++++ SETUP PYTHON ENVS +++++++++++++++
 
 # Install necessary Python packages
 # Note that 'source' is deprecated, so now we should be using 'conda' to activate/deactivate envs
-conda activate ada38
+conda activate wsgi38
 #conda install -y -c conda-forge awscli
 conda install -y -c anaconda urllib3 
 conda install -y -c anaconda certifi 
@@ -49,17 +37,14 @@ conda install -y -c conda-forge dash
 conda install -y -c anaconda beautifulsoup4
 conda install -y -c conda-forge dash-bootstrap-components 
 conda install -y -c conda-forge dash-daq
+conda install -y -c anaconda gunicorn
 
 #save current environment settings
 conda env export --no-builds > /home/ubuntu/environment.yml
 
-### Setup the credentials for the AWS CLI
-##aws configure set aws_access_key_id $1
-##aws configure set aws_secret_access_key $2
+# ++++++++++++++ END SETUP PYTHON ENVS +++++++++++++++
 
-# ++++++++++++++ END SETUP ENV +++++++++++++++
-
-# ++++++++++++++ SETUP CODE +++++++++++++++
+# ++++++++++++++ SETUP APP CODE +++++++++++++++
 cd /home/ubuntu
 mkdir plotlydash
 cd plotlydash
@@ -74,8 +59,9 @@ echo "#/bin/bash" >/home/ubuntu/start.sh
 echo "# code assumes to be run from within plotlydash/code folder">>/home/ubuntu/start.sh
 echo "cd /home/ubuntu/plotlydash/code">>/home/ubuntu/start.sh
 echo "ls -la">>/home/ubuntu/start.sh
-echo "/home/ubuntu/anaconda3/envs/ada38/bin/python3 --version">>/home/ubuntu/start.sh
-echo "/home/ubuntu/anaconda3/envs/ada38/bin/python3 app.py">>/home/ubuntu/start.sh
+echo "/home/ubuntu/anaconda3/envs/wsgi38/bin/python3 --version">>/home/ubuntu/start.sh
+echo "/home/ubuntu/anaconda3/envs/wsgi38/bin/python3 app.py">>/home/ubuntu/start.sh
 chmod +x start.sh
+# ++++++++++++++ END SETUP APP CODE +++++++++++++++
 
-# ++++++++++++ END  +++++++++++++
+# ++++++++++++ THE END  +++++++++++++
